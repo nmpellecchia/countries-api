67c8bbd4c70538bc434b97546412bac2dbbba95c
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-10-12 16:48:43 -0300

    pagination working

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ import { Header } from './components/header/header.js';
 import { MainBody } from './components/main/MainPage.js';
 
 function App() {
+  /* const MAX_COUNTRIES = 25; */
+  const getNumberOfPages = () => {
+    let amountOfPages = 0;
+    for (let i = 0; i < localStorage.length; i++) {
+      const regex = /(countries_)/gi;
+      if (localStorage.key(i).match(regex)) {
+        amountOfPages++;
+      }
+    }
+    return amountOfPages;
+  };
+
   // useEffect is used to execute things that don't directly affect outputs
   useEffect(() => {
     getAllCountries();
@@ -18,7 +30,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MainBody></MainBody>
+      <MainBody pages={getNumberOfPages()}></MainBody>
     </div>
   );
 }
--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -1,22 +1,81 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
-import PageItem from 'react-bootstrap/PageItem';
 
 // This component will handle the children (Pagination and countries)
 // So it will have the logic
 
 export function MainBody(props) {
-  let items = [];
-  for (let number = 1; number <= props.pages; number++) {
-    items.push(
-      <Pagination.Item key={number} active={false}>
-        {number}
-      </Pagination.Item>
-    );
-  }
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <div>
-      <Pagination>{items}</Pagination>
+      <PagesList
+        pages={parseInt(props.pages)}
+        currentPage={currentPage}
+        clickHandler={setCurrentPage}
+      ></PagesList>
+    </div>
+  );
+}
+
+function PagesList(props) {
+  const [pageAmount, setPageAmount] = useState([]);
+
+  useEffect(() => {
+    const pagesTotal = parseInt(props.pages);
+    const amount = [];
+    for (let num = 1; num <= pagesTotal; num++) {
+      amount.push(num);
+    }
+    setPageAmount(amount);
+  }, [props.pages]);
+
+  return (
+    <div>
+      <Pagination>
+        {pageAmount.map((el, i) => {
+          let items = [];
+
+          if (i === 0) {
+            items.push(
+              <Pagination.Prev
+                key={'page-prev'}
+                disabled={props.currentPage > 1 ? false : true}
+                onClick={e => {
+                  e.preventDefault();
+                  props.clickHandler(props.currentPage - 1);
+                }}
+              />
+            );
+          }
+          items.push(
+            <Pagination.Item
+              key={i}
+              /* id={i} */
+              active={props.currentPage === el ? true : false}
+              onClick={e => {
+                e.preventDefault();
+                props.clickHandler(e.target.id);
+              }}
+            >
+              {i + 1}
+            </Pagination.Item>
+          );
+          if (i === pageAmount.length - 1) {
+            items.push(
+              <Pagination.Next
+                key={'page-next'}
+                disabled={props.currentPage < props.pages ? false : true}
+                onClick={e => {
+                  e.preventDefault();
+                  props.clickHandler(props.currentPage + 1);
+                }}
+              />
+            );
+          }
+          return items;
+        })}
+      </Pagination>
     </div>
   );
 }
